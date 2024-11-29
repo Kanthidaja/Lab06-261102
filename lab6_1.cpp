bcc4a225f98e23fa71fdaddb042c3dfc874bee1e
kanthidaja <Kanthida_khun@cmu.ac.th>
2024-11-29 16:27:22 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
+    int x = 1,odd=0,even= 0;
+    while(x!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+        if(x%2==0 && x != 0){
+            even = even+1;
+        }
+        if(x%2==1 && x != 0){
+            odd = odd+1;
+        }
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
